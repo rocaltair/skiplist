1403481e599cbb17f616eb3901ba350096570059
rocaltair <rocaltair@gmail.com>
2017-06-15 16:35:40 +0800

    update benchmark

--- lua-bind/benchmark.lua
+++ lua-bind/benchmark.lua
@@ -4,11 +4,7 @@ function comp(a, b, scoreA, scoreB, pdiff)
 	return scoreA - scoreB
 end
 
-local map_len = 1e5
 local map = {}
-for i = 1, map_len do
-	map[i] = math.random()
-end
 
 function benchmark(f, ...)
 	local s = os.clock()
@@ -18,21 +14,26 @@ end
 
 local sl = lskiplist.new(comp)
 
-function insert()
-	local len = #map
+function insert(len)
 	for i=1, len do
 		sl:insert(i, map[i])
 	end
 end
 
-function rank_range(count, range)
+function update(len)
+	for i=1, len do
+		sl[i] = map[i]
+	end
+end
+
+function rank_range(count, map_len, range)
 	for j = 1, count do
 		local s = math.random(1, map_len - range)
 		local list = sl:rank_range(s, s + range)
 	end
 end
 
-function rank_of(test_count)
+function rank_of(test_count, map_len)
 	for i = 1, test_count do
 		local s = math.random(1, map_len)
 		sl:rank_of(s)
@@ -46,8 +47,20 @@ function delete()
 	end
 end
 
-print("insert", benchmark(insert))
-print("rank_range", benchmark(rank_range, 1e5, 50))
-print("rank_of", benchmark(rank_of, 1e5))
-print("delete", benchmark(delete))
+function printf(fmt, ...)
+	print(string.format(fmt, ...))
+end
 
+function main(args)
+	local map_len = tonumber(args and args[1]) or 1e5
+	for i = 1, map_len do
+		map[i] = math.random()
+	end
+	printf("insert, create() size=%d, time=%.5f", map_len, benchmark(insert, map_len))
+	printf("update, sl:size() == %d, update cnt=%d time=%.5f", map_len, map_len, benchmark(update, map_len))
+	printf("rank_range sl:size() == %d,cnt=%d(x, x+50) time=%.5f", map_len, map_len, benchmark(rank_range, map_len, map_len, 50))
+	printf("rank_of sl:size() == %d,time=%.5f", map_len, benchmark(rank_of, map_len, map_len))
+	printf("delete sl:size() == %d,time=%.5f", map_len, benchmark(delete))
+end
+
+main(arg)
